e2a7832e6f1bc76b7df6dd8eccd9cda4f4609e50
Pulkit Kathuria <kevincobain2000@USER-no-MacBook-Air.local>
2013-08-10 15:18:31 +0900

    Pushing structure + data files

---
+++ main.js
@@ -0,0 +1,13 @@
+var Edict = require("./lib/main.js");
+exports.module = Edict;
+
+//edict = new Edict;
+//edict.redisSetEntries(function(callback){
+//    
+//});
+
+//edict.setDB(2);
+//edict.get('匜;半挿',function(callback){
+//    console.log(callback.Gloss);
+//});
+//
